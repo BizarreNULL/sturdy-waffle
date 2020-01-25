d105869eb3982090ad5ccc4af99a63064b669dba
Jojo <jonas.uliana@passwd.com.br>
2020-01-25 15:40:08 -0300

    Added Sturdy.Waffle.Console console project

---
+++ src/Sturdy.Waffle.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sturdy.Waffle.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
