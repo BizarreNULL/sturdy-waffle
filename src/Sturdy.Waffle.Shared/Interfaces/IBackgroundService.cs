7d62a5d147b460127a9879aca867fda613a74e92
Jojo <jonas.uliana@passwd.com.br>
2020-01-27 09:28:58 -0300

    Added IBackgroundService.cs

---
+++ src/Sturdy.Waffle.Shared/Interfaces/IBackgroundService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Hosting;
+
+namespace Sturdy.Waffle.Shared.Interfaces
+{
+    public interface IBackgroundService : IHostedService, IDisposable
+    {
+        Task ExecuteAsync(CancellationToken stoppingToken);
+    }
+}
