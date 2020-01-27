16feae0a8c13ddb0e6719bdfadb3df1328f2c16d
Jojo <jonas.uliana@passwd.com.br>
2020-01-27 09:40:24 -0300

    Updated entry point for console behaviour

--- src/Sturdy.Waffle.Console/Program.cs
+++ src/Sturdy.Waffle.Console/Program.cs
@@ -1,13 +1,15 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using System.Threading.Tasks;
+
+using Microsoft.Extensions.Hosting;
 
 namespace Sturdy.Waffle.Console
 {
     internal class Program
     {
-        private static void Main(string[] args) => Host.CreateDefaultBuilder(args)
+        private static async Task Main(string[] args) => await Host.CreateDefaultBuilder(args)
             .ConfigureServices(services =>
             {
                 //services.AddHostedService<Something>();
-            });
+            }).RunConsoleAsync();
     }
 }
