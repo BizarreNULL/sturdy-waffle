6b141b10ab0a23e619ed1646aa12c688247bcd1a
Jojo <jonas.uliana@passwd.com.br>
2020-01-27 10:01:28 -0300

    Added configuration file for service

--- src/Sturdy.Waffle.Console/Program.cs
+++ src/Sturdy.Waffle.Console/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System.Threading.Tasks;
 
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace Sturdy.Waffle.Console
 {
     internal class Program
     {
+        private static IConfiguration _configuration;
+        private static IHostEnvironment _environment;
+
         private static async Task Main(string[] args) => await Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                var builder = new ConfigurationBuilder()
+                    .AddJsonFile("Sturdy.Waffle.Configuration.json");
+                _configuration = builder.Build();
+
+                _environment = hostingContext.HostingEnvironment;
+            })
             .ConfigureServices(services =>
             {
-                //services.AddHostedService<Something>();
-            }).RunConsoleAsync();
+
+            })
+            .RunConsoleAsync();
     }
 }
