e9118a92e610ef07de6eb341508263c2c80dceef
Jojo <jonas.uliana@passwd.com.br>
2020-01-27 10:17:34 -0300

    Added Paste model

---
+++ src/Sturdy.Waffle.Shared/Models/Paste.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sturdy.Waffle.Shared.Models
+{
+    [Table("Pastes")]
+    public class Paste
+    {
+        [Key] public string Id { get; set; }
+
+        [Required] public DateTime Date { get; set; }
+
+        [Required, StringLength(maximumLength: 9)]
+        public string Key { get; set; }
+
+        [Required] public int Size { get; set; }
+
+        [Required] public string Syntax { get; set; }
+
+        public DateTime Expire { get; set; }
+
+        public string Title { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
